c51bd8f6dc49a22b13779a9dc9892302cf60614e
Dendō <1886642+d3nd0@users.noreply.github.com>
2026-04-22 23:39:59 +1000

    Remove reddit watch, add repo info file
    RedditWatch exists elsewhere since development so it's not needed and was never finished.

--- redditwatch/__init__.py
+++
@@ -1,8 +0,0 @@
-from .redditwatch import redditwatch
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    cog = redditwatch(bot)
-    await bot.add_cog(cog)
